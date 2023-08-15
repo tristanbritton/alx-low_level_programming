73e71f29c750a925d47c1d8a5c638f85e128dc81
Tristan Britton <tristanbritton01@gmail.com>
2023-08-15 11:29:35 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+	  * main - print put_char
+	  * Return: 0
+*/
+
+int main(void)
+{
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('_');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
+
