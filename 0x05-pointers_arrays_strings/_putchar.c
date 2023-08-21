87e578604a2fd5c112add17d38a4a6e536c7eddf
Tristan Britton <tristanbritton01@gmail.com>
2023-08-21 12:48:47 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>  /* Include the necessary header for the write function */
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
